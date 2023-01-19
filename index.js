30ae774587ee944a730d54f26da11aaa924b8aef
Hariom Kumar <hariom.star.hk@gmail.com>
2023-01-19 09:42:01 +0530

    cors added in dependency

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const multer = require("multer");
 const path = require("path");
 const PORT = process.env.PORT || 5000
 
+const cors  = require("cors");
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+
 dotenv.config(); 
 app.use(express.json());
 // app.use("/images", express.static(path.join(__dirname, "/images")));
